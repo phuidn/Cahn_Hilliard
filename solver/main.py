164da8b509107d72cc2b53e86f1e08261083258b
phuidn <robertwilson1024@gmail.com>
2017-05-06 23:40:25 +0100

    user command line input added and tested

--- solver/main.py
+++ solver/main.py
@@ -1,5 +1,46 @@
+"""
+Basic functions for Cahn-Hilliard solver
+"""
+
+import argparse
+import sys
 
 
-def main():
-    print "hello"
+def main(argv):
+    """The Main function, calls everything else
+    """
+
+    parameters = read_input(argv)
+
     return 0
+
+
+def read_input(argv):
+    """Reads command line input provided by user.
+
+    Args:
+
+    Returns:
+        A dict mapping given args to their values
+    """
+
+    parser = argparse.ArgumentParser(description="Integrate the 2d Cahn-Hilliard equation")
+
+    parser.add_argument("--timesteps", type=int, default=1000,
+                        help="set the number of timesteps to run the simulation for")
+    parser.add_argument("--n_elements", type=int, default=250,
+                        help="set the number of elements N in the NxN domain")
+    parser.add_argument("--periodic", help="Use periodic boundaries",
+                        action="store_true")
+    parser.add_argument("--diffusion_coeff", type=float, default=1.0,
+                        help="set the diffusion coefficient")
+    parser.add_argument("--boundary_coeff", type=float, default=1.0,
+                        help="set the boundary coefficient")
+
+    arguments = parser.parse_args(argv)
+
+    return vars(arguments)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- test/unit_test.py
+++ test/unit_test.py
@@ -4,4 +4,22 @@ from solver import main
 class CahnHilliardTests(unittest.TestCase):
 
     def test_main(self):
-        main.main()
+        """ Test the main function
+        """
+        
+        main.main([])
+    
+    def test_read_input(self):
+        """ Test the read input function
+        """
+
+        args = ["--timesteps", "500", "--n_elements", "50", "--periodic", "--diffusion_coeff", "1.5", "--boundary_coeff", "1.5"]
+        input_dict = main.read_input(args)
+
+        self.assertEqual(input_dict["timesteps"], 500)
+        self.assertEqual(input_dict["n_elements"], 50)
+        self.assertEqual(input_dict["periodic"], True)
+        self.assertEqual(input_dict["diffusion_coeff"], 1.5)
+        self.assertEqual(input_dict["boundary_coeff"], 1.5)
+
+
