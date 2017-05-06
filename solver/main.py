3ee75e353887b57225451e8d00b0490bcfa287bb
phuidn <robertwilson1024@gmail.com>
2017-05-06 20:52:25 +0100

    first test passed! module loads correctly and runs main function

---
+++ solver/main.py
@@ -0,0 +1,5 @@
+
+
+def main():
+    print "hello"
+    return 0
---
+++ test/unit_test.py
@@ -0,0 +1,7 @@
+import unittest
+from solver import main
+
+class CahnHilliardTests(unittest.TestCase):
+
+    def test_main(self):
+        main.main()
