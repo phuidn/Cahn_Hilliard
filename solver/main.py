fa6cb3e19e474e9e7dc6960bd5c8676ae862957c
phuidn <robertwilson1024@gmail.com>
2017-05-07 12:24:29 +0100

    started some of the actual maths

--- solver/main.py
+++ solver/main.py
@@ -5,14 +5,28 @@ Basic functions for Cahn-Hilliard solver
 import argparse
 import sys
 import numpy as np
+import scipy.ndimage.filters as filters
 
 def main(argv):
     """The Main function, calls everything else
     """
 
+    # read command line inputs
     parameter_dict = read_input(argv)
 
+    # generate initial system configuration
     system_array = generate_initial_config(parameter_dict)
+
+    n_timesteps = parameter_dict["timesteps"]
+
+    # main simulation loop over timesteps
+    for i in xrange(n_timesteps):
+
+        pass
+
+        # calculate system for the current timestep
+
+        # output data
 
     return 0
 
@@ -32,14 +46,20 @@ def read_input(argv):
 
     parser.add_argument("--timesteps", type=int, default=1000,
                         help="set the number of timesteps to run the simulation for")
+    parser.add_argument("--timestep_value", type=float, default=0.0001,
+                        help="set the duration of each timestep")
     parser.add_argument("--n_elements", type=int, default=250,
                         help="set the number of elements N in the NxN domain")
     parser.add_argument("--periodic", help="Use periodic boundaries",
-                        action="store_true")
-    parser.add_argument("--diffusion_coeff", type=float, default=1.0,
+                        dest="boundary", action="store_const", 
+                        const="wrap", default="reflect")
+    parser.add_argument("--diffusion_coeff", type=float, default=0.5,
                         help="set the diffusion coefficient")
-    parser.add_argument("--boundary_coeff", type=float, default=1.0,
+    parser.add_argument("--boundary_coeff", type=float, default=0.5,
                         help="set the boundary coefficient")
+    parser.add_argument("--output", type=str, default="default", 
+                        choices=["plot", "print", "none"],
+                        help="set the output format")
 
     arguments = parser.parse_args(argv)
 
@@ -60,6 +80,25 @@ def generate_initial_config(parameter_dict):
     dimension = parameter_dict["n_elements"]
 
     return np.random.ranf((dimension, dimension)) * 2.0 - 1.0
+
+def calculate_du_dt(system, parameter_dict):
+    """ Calculate the time derivative of the system in its current state
+
+    Args:
+        system - the current system, a 2d numpy array
+        parameter_dict - the dictionary of parameters defined by the user
+
+    Returns
+        a numpy array of the same dimension as the system
+    """
+
+    D_coeff = parameter_dict["diffusion_coeff"]
+    b_coeff = parameter_dict["boundary_coeff"]
+    
+    potential_component = system**3 - system
+    boundary_component = b_coeff * filters.laplace(system, mode = parameter_dict["boundary"])
+
+    return D_coeff * filters.laplace(potential_component + boundary_component, mode = parameter_dict["boundary"])
 
 
 if __name__ == "__main__":
--- test/unit_test.py
+++ test/unit_test.py
@@ -8,26 +8,38 @@ class CahnHilliardTests(unittest.TestCase):
         """ Test the main function
         """
         
-        main.main([])
+        # run a short simulation
+        main.main(["--timesteps", "10", 
+                   "--n_elements", "50", 
+                   "--output", "none"])
     
     def test_read_input(self):
         """ Test the read_input function
         """
 
-        args = ["--timesteps", "500", "--n_elements", "50", "--periodic", "--diffusion_coeff", "1.5", "--boundary_coeff", "1.5"]
+        args = ["--timesteps", "500", 
+                "--timestep_value", "0.001",
+                "--n_elements", "50", 
+                "--periodic", 
+                "--diffusion_coeff", "1.5", 
+                "--boundary_coeff", "1.5", 
+                "--output", "plot"]
+
         input_dict = main.read_input(args)
 
         self.assertEqual(input_dict["timesteps"], 500)
         self.assertEqual(input_dict["n_elements"], 50)
-        self.assertEqual(input_dict["periodic"], True)
+        self.assertEqual(input_dict["boundary"], "wrap")
         self.assertEqual(input_dict["diffusion_coeff"], 1.5)
         self.assertEqual(input_dict["boundary_coeff"], 1.5)
+        self.assertEqual(input_dict["output"], "plot")
+        self.assertEqual(input_dict["timestep_value"], 0.001)
 
     def test_generate_initial_config(self):
         """ Test the generate_initial_config function
         """
 
-        input_dict = main.read_input([])
+        input_dict = {"n_elements": 250}
         system_array = main.generate_initial_config(input_dict)
         
         # array is the correct size
@@ -37,3 +49,18 @@ class CahnHilliardTests(unittest.TestCase):
         # all array values are valid
         self.assertTrue(np.max(system_array) < 1.0)
         self.assertTrue(np.min(system_array) > -1.0)
+    
+    def test_calculate_du_dt(self):
+        """ Test the calculation of the time derivative of the concentration
+        """
+        
+        input_dict = main.read_input([])
+        initial_array = main.generate_initial_config(input_dict)
+
+        new_array = main.calculate_du_dt(initial_array, input_dict)
+
+        end_array = initial_array + input_dict["timestep_value"] * new_array
+
+        #self.assertTrue(np.max(end_array) < 1.0)
+        #self.assertTrue(np.min(end_array) > -1.0)
+
